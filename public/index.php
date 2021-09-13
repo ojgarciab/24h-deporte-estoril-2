b09e80be69d71de154a9338ef851637ff2541274
Oscar García <oscar.github@linaresdigital.com>
2021-09-13 20:44:19 +0000

    Versión inicial

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Página principal</title>
+  </head>
+  <body>
+    <section>
+      <h1>Título</h1>
+      <p>Página por defecto.</p>
+    </section>
+  </body>
+</html>
